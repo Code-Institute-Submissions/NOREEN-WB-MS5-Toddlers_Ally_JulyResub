64239b24e0f23cc8c77cabb5a2e9bfc51dc6ea5c
noreen-wb <nwbutt@hotmail.com>
2022-05-30 16:34:48 +0500

    secret ley

--- toddlers_ally/settings.py
+++ toddlers_ally/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!rcn3$oju)4*jlti)cd$+jk43j0p-g-t=6#f1kxp$()c1f4dg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
