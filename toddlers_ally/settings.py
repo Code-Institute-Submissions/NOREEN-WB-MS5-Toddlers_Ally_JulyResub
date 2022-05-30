676c9bf1ffb11ea83e0b158a76684e029f7b85ef
noreen-wb <nwbutt@hotmail.com>
2022-05-30 17:50:58 +0500

    static files / media files / aws Bucket config

--- toddlers_ally/settings.py
+++ toddlers_ally/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'django_summernote',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -178,6 +179,24 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'toddlers-ally'
+    AWS_S3_REGION_NAME = 'ap-south-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT   = os.path.join(BASE_DIR, 'media')
